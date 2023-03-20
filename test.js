8041fa919c25b23056a51a340934c89c3aca8d78
Sllekgeek254 <tchiwo@icloud.com>
2023-03-20 11:54:00 +0300

    initial commit

---
+++ test.js
@@ -0,0 +1,3 @@
+// function getAmountIn(uint amountOut, uint reserveIn, uint reserveOut) external pure returns (uint amountIn);
+
+// function getAmountIn(uint amountOut, uint reserveIn, uint reserveOut) external pure returns (uint amountIn);
